45d5d612f102f79ce9c5518ee1434ea7ce7a8d43
Andrey-PC\Andrey <Pirynas@mail.ru>
2019-10-13 13:03:03 +0700

    Прятанье в кустах и траве

---
+++ Assets/Scripts/AvatarControllers/HideSelf.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Прячется полностью пока в убежище
+/// </summary>
+[RequireComponent(typeof(AvatarControl))]
+public class HideSelf : MonoBehaviour, IHidable
+{
+    private GameObject avatar;
+
+    public void HideObject()
+    {
+        avatar.SetActive(false);
+    }
+
+    public void UnHideObject()
+    {
+        avatar.SetActive(true);
+    }
+
+    private void Awake()
+    {
+        GetComponent<AvatarControl>().OnAvatarSpawn += Init;
+    }
+
+    private void Init(GameObject _avatar)
+    {
+        avatar = _avatar;
+    }
+}
---
+++ Assets/Scripts/EvironmentControllers/HideoutController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideoutController : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        IHidable hidable = other.GetComponent<IHidable>();
+        if(hidable != null)
+        {
+            hidable.HideObject();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        IHidable hidable = other.GetComponent<IHidable>();
+        if (hidable != null)
+        {
+            hidable.UnHideObject();
+        }
+    }
+}
