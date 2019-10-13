8698a4e15735dd6a576f7ae42936c86d60f838d7
Andrey-PC\Andrey <Pirynas@mail.ru>
2019-10-13 13:03:13 +0700

    Кастомные локации

---
+++ Assets/Scripts/EvironmentControllers/LocationData.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Информация о локации
+/// </summary>
+[CreateAssetMenu(fileName = "LocationData", menuName = "ScriptableObject/Location Data", order = 51)]
+public class LocationData : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject locationPrefab;
+
+    /// <summary>
+    /// Префаб аватара
+    /// </summary>
+    public GameObject LocationPrefab
+    {
+        get
+        {
+            return locationPrefab;
+        }
+    }
+
+    [SerializeField]
+    private GameObject locationName;
+
+    /// <summary>
+    /// Префаб аватара
+    /// </summary>
+    public GameObject LocationName
+    {
+        get
+        {
+            return locationName;
+        }
+    }
+}
