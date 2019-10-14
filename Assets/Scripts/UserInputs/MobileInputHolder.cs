29166b8e5fe2038320f084bf5f3d8b4736476a67
Andrey-PC\Andrey <Pirynas@mail.ru>
2019-10-14 19:51:48 +0700

    скрипты джойстиков

---
+++ Assets/Scripts/UserInputs/MobileInputHolder.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Держатель джойстиков мобильного управления
+/// </summary>
+public class MobileInputHolder : MonoBehaviour
+{
+    [SerializeField]
+    private JoystickController leftJoystickController;
+    [SerializeField]
+    private JoystickController rightJoystickController;
+
+    public JoystickController LeftJoystick
+    {
+        get
+        {
+            return leftJoystickController;
+        }
+    }
+
+    public JoystickController RightJoystick
+    {
+        get
+        {
+            return rightJoystickController;
+        }
+    }
+}
--- Assets/Scripts/UserInputs/UserControlInput.cs
+++ Assets/Scripts/UserInputs/UserControlInput.cs
@@ -41,8 +41,8 @@ public class UserControlInput : MonoBehaviour
 
     private void Start()
     {
-#if UNITY_EDITOR || UNITY_STANDALONE
-        userInput = gameObject.AddComponent<DesktopUserInput>();
-#endif      
-    }    
+        userInput = gameObject.GetComponent<IUserInput>();
+
+        this.enabled = userInput != null;
+    }
 }
